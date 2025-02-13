71b4c25f517c88f4f9a2fa59f464197613a83fdd
arfanur04 <arfanur04@gmail.com>
2025-02-13 17:29:20 +0600

    blog page added

--- js/index.js
+++ js/index.js
@@ -1,37 +1,45 @@
-(function () {
-	const donationBtn = document.getElementById("show-donation");
-	const historyBtn = document.getElementById("show-history");
+const blogElements = document.querySelectorAll(
+	"#blog-mobile-device, #blog-large-device"
+);
 
-	donationBtn.addEventListener("click", function () {
-		hide_Show_Sec("transaction-history", "donation-container");
-
-		donationBtn.classList.remove("border-gray-200");
-		donationBtn.classList.add(
-			"border-donatePrimaryColor",
-			"bg-donatePrimaryColor"
-		);
-		historyBtn.classList.remove(
-			"border-donatePrimaryColor",
-			"bg-donatePrimaryColor"
-		);
-		historyBtn.classList.add("border-gray-200");
+blogElements.forEach((blogEl) => {
+	blogEl.addEventListener("click", function () {
+		window.location.href = "blog.html";
 	});
+});
 
-	historyBtn.addEventListener("click", function () {
-		hide_Show_Sec("donation-container", "transaction-history");
+const donationBtn = document.getElementById("show-donation");
+const historyBtn = document.getElementById("show-history");
 
-		donationBtn.classList.remove(
-			"border-donatePrimaryColor",
-			"bg-donatePrimaryColor"
-		);
-		donationBtn.classList.add("border-gray-200");
-		historyBtn.classList.remove("border-gray-200");
-		historyBtn.classList.add(
-			"border-donatePrimaryColor",
-			"bg-donatePrimaryColor"
-		);
-	});
-})();
+donationBtn.addEventListener("click", function () {
+	hide_Show_Sec("transaction-history", "donation-container");
+
+	donationBtn.classList.remove("border-gray-200");
+	donationBtn.classList.add(
+		"border-donatePrimaryColor",
+		"bg-donatePrimaryColor"
+	);
+	historyBtn.classList.remove(
+		"border-donatePrimaryColor",
+		"bg-donatePrimaryColor"
+	);
+	historyBtn.classList.add("border-gray-200");
+});
+
+historyBtn.addEventListener("click", function () {
+	hide_Show_Sec("donation-container", "transaction-history");
+
+	donationBtn.classList.remove(
+		"border-donatePrimaryColor",
+		"bg-donatePrimaryColor"
+	);
+	donationBtn.classList.add("border-gray-200");
+	historyBtn.classList.remove("border-gray-200");
+	historyBtn.classList.add(
+		"border-donatePrimaryColor",
+		"bg-donatePrimaryColor"
+	);
+});
 
 document
 	.getElementById("donate-noakhali")
